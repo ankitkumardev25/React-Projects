c71fd82da1c7024dd5d94ba29f274546a760d91d
ankitkumardev25 <ankitkumar1999825@gmail.com>
2023-08-23 22:33:30 +0530

    02 and 03

---
+++ 02-best-tours-app/src/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <h3>please wait while we get the best deals for you... 😊❤️</h3>
+    </div>
+  )
+}
+
+export default Loading
---
+++ 03-honest-reviews-app/src/data.js
@@ -0,0 +1,44 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'Ranbir Kapoor',
+    job: 'web developer',
+    image:
+      'https://www.themoviedb.org/t/p/w500/ymYNHV9luwgyrw17NXHqbOWTQkg.jpg',
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: 'Deepika Padukone',
+    job: 'web designer',
+    image:
+      'https://img.indiaforums.com/person/640x480/0/2576-deepika-padukone.jpg?c=7rPD3F',
+    text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+  },
+  {
+    id: 3,
+    name: 'Alia Bhatt',
+    job: 'intern',
+    image:
+      'https://m.media-amazon.com/images/M/MV5BNzlmZWI2YjAtOGQ3ZC00NTFjLThkMzItYTRiOGFiNzFjOTdlXkEyXkFqcGdeQXVyMTQ3Mzk2MDg4._V1_.jpg',
+    text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+  },
+  {
+    id: 4,
+    name: 'Ranveer Singh',
+    job: 'Manager',
+    image:
+      'https://bsmedia.business-standard.com/_media/bs/img/article/2019-02/12/full/1549988381-1385.jpg',
+    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+  {
+    id: 5,
+    name: 'Priyanka Chopra',
+    job: 'Tech Lead',
+    image:
+      'https://media.vogue.co.uk/photos/63c7cfeabf3a6a0840b97806/2:3/w_2560%2Cc_limit/P4HKR9.jpg',
+    text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+  },
+]
+
+export default reviews
