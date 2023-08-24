95c3effc4b0242af82c85734057434008a17fecc
ankitkumardev25 <ankitkumar1999825@gmail.com>
2023-08-24 15:34:39 +0530

    04-Questions and Answers

---
+++ 04-accordion/src/data.js
@@ -0,0 +1,61 @@
+const questions = [
+  {
+    id: 1,
+    title: 'What is the correct command to create a new React project?',
+    info: 'npx create-react-app myReactApp',
+  },
+  {
+    id: 2,
+    title:
+      'What does myReactApp refer to in the following command? \n\n npx create-react-app myReactApp',
+    info: 'The name you want to use for the new app ',
+  },
+  {
+    id: 3,
+    title: 'What command is used to start the React local development server?',
+    info: 'npm start  ',
+  },
+  {
+    id: 4,
+    title:
+      'What is the default local host port that a React development server uses?',
+    info: '3000 ',
+  },
+  {
+    id: 5,
+    title: 'To develop and run React code, Node.js is required?',
+    info: 'TRUE',
+  },
+  {
+    id: 6,
+    title: 'What is the children prop?',
+    info: 'A property that lets you nest components in other components ',
+  },
+  {
+    id: 7,
+    title: 'Which keyword creates a constant in JavaScript?',
+    info: 'const',
+  },
+  {
+    id: 8,
+    title: 'A copy of the real DOM that is kept in memory is called what?',
+    info: 'Virtual DOM',
+  },
+  {
+    id: 9,
+    title: 'React component names must begin with an uppercase letter?',
+    info: 'TRUE',
+  },
+  {
+    id: 10,
+    title:
+      'Which operator can be used to conditionally render a React component?',
+    info: '&&',
+  },
+  {
+    id: 11,
+    title: 'What tool does React use to compile JSX?',
+    info: 'Babel',
+  },
+]
+export default questions
