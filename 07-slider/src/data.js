bf54248d25e1899b162f06efb09785b6fc46947f
ankitkumardev25 <ankitkumar1999825@gmail.com>
2023-08-29 23:56:19 +0530

    07-slider reviews

---
+++ 07-slider/src/data.js
@@ -0,0 +1,40 @@
+const people = [
+  {
+    id: 1,
+    image:
+      'https://www.bollywoodhungama.com/wp-content/uploads/2019/07/Akshay-Kumar-is-the-only-Indian-actor-to-feature-in-Forbes-World%E2%80%99s-Highest-Paid-Entertainers-2019.jpg',
+    name: 'Akshay Kumar',
+    title: 'office manager',
+    quote:
+      'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+  },
+  {
+    id: 2,
+    image:
+      'https://mir-s3-cdn-cf.behance.net/project_modules/hd/3b618e60298373.5a461919b15fd.jpg',
+    name: 'Salman Khan',
+    title: 'regular guy',
+    quote:
+      'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
+  },
+  {
+    id: 3,
+    image:
+      'https://c.saavncdn.com/artists/Shahid_Kapoor_003_20230608090033_500x500.jpg',
+    name: 'Shahid Kapoor',
+    title: 'product designer',
+    quote:
+      'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
+  },
+  {
+    id: 4,
+    image:
+      'https://i.pinimg.com/236x/63/6d/68/636d68693b0c2f9b0914fa24bd6398a9.jpg',
+    name: 'Hrithik Roshan',
+    title: 'the boss',
+    quote:
+      'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
+  },
+]
+
+export default people
